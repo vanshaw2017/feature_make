99b4801a730569eeb3a72a2fac305506a7f6b49e
XiaoXiaoFan1995 <614664248@qq.com>
2018-11-06 15:08:49 +0800

    modify the function

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import math
-index_array=[' gaze_0_x',' gaze_0_y',' gaze_0_z',' gaze_1_x',' gaze_1_y',' gaze_1_z',' gaze_angle_x',' gaze_angle_y',
+face_index_array=[' gaze_0_x',' gaze_0_y',' gaze_0_z',' gaze_1_x',' gaze_1_y',' gaze_1_z',' gaze_angle_x',
+                 ' gaze_angle_y',
              ' eye_0_distace',' eye_1_distace',' eye_lmk_Z_11',' eye_lmk_Z_17',' eye_lmk_Z_39',' eye_lmk_Z_45',
              ' pose_Tx',
              ' pose_Ty',' pose_Tz',' pose_Rx', ' pose_Ry', ' pose_Rz',' X_0',' X_4',' X_8',' X_12',' X_16',' Y_0',
@@ -14,21 +15,31 @@ index_array=[' gaze_0_x',' gaze_0_y',' gaze_0_z',' gaze_1_x',' gaze_1_y',' gaze_
              ' AU25_c', ' AU26_c',' AU28_c', ' AU45_c'
             ]
 
-#  def cal_distance(df):
-#     df[' eye_0_distace'] = math.sqrt((df[' eye_lmk_X_11']-df[' eye_lmk_X_17'])**2+
-#                                      (df[' eye_lmk_Y_11']-df[' eye_lmk_Y_17'])**2 )
-#     df[' eye_1_distace'] = math.sqrt((df[' eye_lmk_X_39'] - df[' eye_lmk_X_45']) ** 2 +
-#                                      (df[' eye_lmk_Y_39'] - df[' eye_lmk_Y_45']) ** 2)
-#     return df
+pose_index_array=['Nose_x','Nose_y','Neck_x','Neck_y','RShoulder_x','RShoulder_y','RElbow_x','RElbow_y','RWrist_x',
+                  'RWrist_y','LShoulder_x','LShoulder_y','LElbow_x','LElbow_y','LWrist_x','LWrist_y']
+def openface_feature_process(face_feature_input_path,pose_feature_input_path,face_index_array,pose_index_array,
+                             final_feature_path):
+    '''
+    :param feature_input_path:
+    the file path for the feature
+    :return:
+    '''
+    for csv_file in os.listdir(face_feature_input_path):
+        df_face=pd.read_csv(face_feature_input_path+'/'+csv_file)
+        df_pose = pd.read_csv(pose_feature_input_path + '/' + csv_file)
+        #合成新的眼睛特征
+        df_face[' eye_0_distace'] = df_face[' eye_lmk_Y_17'] - df_face[' eye_lmk_Y_11']
+        df_face[' eye_1_distace'] = df_face[' eye_lmk_Y_45'] - df_face[' eye_lmk_Y_39']
+        new_face_index=df_face.reindex(columns=face_index_array)
+        final_face_df = new_face_index.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+        new_pose_index = df_pose.reindex(columns=pose_index_array)
+        final_pose_df = new_pose_index.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+        final_df=final_face_df.join(final_pose_df)
+        final_df.to_csv(final_feature_path+'/'+csv_file,index=0)
 
-for csv_file in os.listdir("./dataset/OpenFace"):
-    df=pd.read_csv("./dataset/OpenFace/"+csv_file)
-    #合成新的眼睛特征
-    df[' eye_0_distace'] = df[' eye_lmk_Y_17'] - df[' eye_lmk_Y_11']
-    df[' eye_1_distace'] = df[' eye_lmk_Y_45'] - df[' eye_lmk_Y_39']
-    new_index=df.reindex(columns=index_array)
-    final_df = new_index.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-    final_df.to_csv("./dataset/OpenFace/"+csv_file,index=0)
+face_feature_input_path="./dataset/OpenFace"
+openface_feature_process("F:/DaiSEE/OpenFaceData/OpenFaceTrain")
+
 
 
 

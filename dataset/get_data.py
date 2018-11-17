e5b887cf65563805e8895c46c113b29ab278f675
XiaoXiaoFan1995 <614664248@qq.com>
2018-11-17 20:11:14 +0800

    modify the function

--- dataset/get_data.py
+++ dataset/get_data.py
@@ -11,52 +11,53 @@ def get_data(feature_path,label_path):
     """
     video_list = []#video in trainlabel.csv
     engagement_label = []#the total labels
-    # data1 = np.zeros((1,86)) #tatal data
+    data1 = np.zeros((1,86)) #tatal data
     # data2 = np.zeros((1, 86))
     # data3 = np.zeros((1, 86))
     # data4 = np.zeros((1, 86))
     # data5 = np.zeros((1, 86))
     df = pd.read_csv(label_path)
-    # for video in df['ClipID']:
-    #     videoID = video.split(".")[0]
-    #     video_list.append(videoID)
+    for video in df['ClipID']:
+        videoID = video.split(".")[0]
+        video_list.append(videoID)
     for video_label in df['Engagement']:
         engagement_label.append(video_label)
     engagement_label = np.array(engagement_label)
-    engagement_label=engagement_label[0:5000]
+    np.save("F:/DaiSEE/Segment/Train/train_labels.npy",engagement_label)
     # to get the whole data
-    # for index in video_list[0:1000]:#5358
-    #     df = pd.read_csv(feature_path + '/' + index + '.csv')
-    #     df = df.as_matrix()
-    #     data1=np.concatenate((data1,df),axis=0)
-    # data1=np.delete(data1, 0, axis=0)
-    # np.save("F:/DaiSEE/data1.npy",data1)
-    # print(data1.shape)
-    #
-    # for index in video_list[1000:2000]:#5358
+    for index in video_list[5000:]:#5358
+        df = pd.read_csv(feature_path + '/' + index + '.csv')
+        df = df.as_matrix()
+        data1=np.concatenate((data1,df),axis=0)
+    data1=np.delete(data1, 0, axis=0)
+    print(data1.shape)
+    np.save("F:/DaiSEE/Segment/Train/data1.npy",data1)
+
+
+    # for index in video_list[500:1000]:#5358
     #     df = pd.read_csv(feature_path + '/' + index + '.csv')
     #     df = df.as_matrix()
     #     data2=np.concatenate((data2,df),axis=0)
     # data2 = np.delete(data2, 0, axis=0)
-    # np.save("F:/DaiSEE/data2.npy", data2)
+    # np.save("F:/DaiSEE/Segment/Validation/data2.npy", data2)
     # print(data2.shape )
-
-    # for index in video_list[2000:3000]:  # 5358
+    #
+    # for index in video_list[1000:]:  # 5358
     #     df = pd.read_csv(feature_path + '/' + index + '.csv')
     #     df = df.as_matrix()
     #     data3 = np.concatenate((data3, df), axis=0)
     # data3 = np.delete(data3, 0, axis=0)
-    # np.save("F:/DaiSEE/data3.npy", data3)
+    # np.save("F:/DaiSEE/Segment/Validation/data3.npy", data3)
     # print(data3.shape)
-    #
-    # for index in video_list[3000:4000]:  # 5358
+
+    # for index in video_list[1500:]:  # 5358
     #     df = pd.read_csv(feature_path + '/' + index + '.csv')
     #     df = df.as_matrix()
     #     data4 = np.concatenate((data4, df), axis=0)
     # data4 = np.delete(data4, 0, axis=0)
-    # np.save("F:/DaiSEE/data4.npy", data4)
+    # np.save("F:/DaiSEE/Segment/Test/data4.npy", data4)
     # print(data4.shape)
-    #
+
     # for index in video_list[4000:5000]:  # 5358
     #     df = pd.read_csv(feature_path + '/' + index + '.csv')
     #     df = df.as_matrix()
@@ -64,33 +65,38 @@ def get_data(feature_path,label_path):
     # data5 = np.delete(data5, 0, axis=0)
     # np.save("F:/DaiSEE/data5.npy", data5)
     # print(data5.shape)
-    data1 = np.load("F:/DaiSEE/data1.npy")
-    data2 = np.load("F:/DaiSEE/data2.npy")
-    data3 = np.load("F:/DaiSEE/data3.npy")
-    data4 = np.load("F:/DaiSEE/data4.npy")
-    data5 = np.load("F:/DaiSEE/data5.npy")
-    data=np.concatenate((data1,data2,data3,data4,data5))
+    data1 = np.load("F:/DaiSEE/Segment/Train/data1.npy")
+    data2 = np.load("F:/DaiSEE/Segment/Train/total_feature_5000.npy")
+    # data2 = np.load("F:/DaiSEE/Segment/Validation/data2.npy")
+    # data3 = np.load("F:/DaiSEE/Segment/Validation/data3.npy")
+    #data4 = np.load("F:/DaiSEE/Segment/Validation/data4.npy")
+    #data5 = np.load("F:/DaiSEE/data5.npy")
+    data1 = data1.reshape((-1, 300, 86))
+    data2 = data2.reshape((-1, 300, 86))
+    data=np.concatenate((data1,data2))
     data=data.reshape((-1,300,86))
+    np.save("F:/DaiSEE/Segment/Train/train_data.npy", data)
+
 
 
 
     #spilt the train set、validation set and test set ,the ratio is 6:2:2
-    X_train_validation, X_test, y_train_validation, y_test = train_test_split(data, engagement_label, test_size=0.2,
-                                                                  random_state=42,shuffle=True)
-    X_train, X_validation, y_train, y_validation = train_test_split(X_train_validation, y_train_validation,
-                                                                    test_size=0.2,random_state=42, shuffle=True)
-    return X_train,y_train,X_validation,y_validation,X_test,y_test
+    # X_train, X_test, y_train, y_test = train_test_split(data, engagement_label, test_size=0.2,
+    #                                                               random_state=42,shuffle=True)
+    # #X_train, X_validation, y_train, y_validation = train_test_split(X_train_validation, y_train_validation,
+    #                                                                 #test_size=0.2,random_state=42, shuffle=True)
+    # return X_train,y_train,X_test,y_test
 
 
-label_path="./TrainLabels.csv"
+
+label_path="F:/DaiSEE/DAiSEE/DAiSEE/Labels/TrainLabels.csv"
 feature_path = "F:/DaiSEE/FinalFeature/Train"
-X_train,y_train,X_validation,y_validation,X_test,y_test=get_data(feature_path,label_path)
+get_data(feature_path,label_path)
 
-print(X_train.shape,X_test.shape,X_validation.shape)
-print(y_train.shape,y_test.shape,y_validation.shape)
-np.save("F:/DaiSEE/X_train.npy", X_train)
-np.save("F:/DaiSEE/X_test.npy", X_test)
-np.save("F:/DaiSEE/X_validation.npy", X_validation)
-np.save("F:/DaiSEE/y_train.npy", y_train)
-np.save("F:/DaiSEE/y_test.npy", y_test)
-np.save("F:/DaiSEE/y_validation.npy", y_validation)
+
+# np.save("F:/DaiSEE/X_train.npy", X_train)
+# np.save("F:/DaiSEE/X_test.npy", X_test)
+# #np.save("F:/DaiSEE/X_validation.npy", X_validation)
+# np.save("F:/DaiSEE/y_train.npy", y_train)
+# np.save("F:/DaiSEE/y_test.npy", y_test)
+# #np.save("F:/DaiSEE/y_validation.npy", y_validation)
